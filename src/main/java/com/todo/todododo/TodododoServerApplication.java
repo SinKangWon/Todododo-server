eba75e2669e69c9c95ca5d264ad5346a8caac633
ShinKangWon <ekdrms5153@naver.com>
2023-08-30 16:43:35 +0900

    create project

---
+++ src/main/java/com/todo/todododo/TodododoServerApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todododo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodododoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodododoServerApplication.class, args);
+	}
+
+}
